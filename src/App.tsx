8fea9cbb47d99a9154e6db8bff37374d9fdeff63
Aleksandr_k <rzn.russia4912@gmail.com>
2023-08-20 09:54:43 +0300

    01 component

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      This is App copmonentw
+    </div>
+  );
+}
+
+function Rating() {
+  return (
+    <div>
+      app world
+    </div>
+  )
+
+}
+export default App;
